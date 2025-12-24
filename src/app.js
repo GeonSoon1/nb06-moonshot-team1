853fc5a0729e3c878ac13832f908c35c04b5a1bb
Sooyoung Chung <chung.sooyoung@gmail.com>
2025-12-24 17:49:51 +0900

    ♻️ chore(update): invitationRouter로 수정

--- src/app.js
+++ src/app.js
@@ -1,21 +1,19 @@
 import express from "express";
 import cookieParser from "cookie-parser";
-import { defaultNotFoundHandler, globalErrorHandler } from "./lib/errors/errorHandler.js";
-import projectRouter from "./routers/project.router.js";
-
-
+import {
+  defaultNotFoundHandler,
+  globalErrorHandler,
+} from "./lib/errors/errorHandler.js";
+import projectRouter from "./routers/invitation.router.js";
 
 const app = express();
-
 
 app.use(express.json());
 app.use(cookieParser());
 
-
 app.use("/projects", projectRouter);
 
-app.use(defaultNotFoundHandler)
-app.use(globalErrorHandler)
+app.use(defaultNotFoundHandler);
+app.use(globalErrorHandler);
 
 app.listen(3000, () => console.log("Server Start"));
-
