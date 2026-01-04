295edb26a1be072992c0e6b2dbc262186d14b70f
Sooyoung Chung <chung.sooyoung@gmail.com>
2026-01-04 12:17:36 +0900

    ♻️ chore(update) : add request variables (baseUrl, accessToken, refreshToiken)

--- src/test.js
+++
@@ -1,8 +0,0 @@
-import bcrypt from 'bcrypt';
-
-for (let i = 1; i < 10; i++) {
-  const password = 'password' + String(i);
-  console.log('');
-  console.log(password);
-  console.log(await bcrypt.hash(password, 10));
-}
