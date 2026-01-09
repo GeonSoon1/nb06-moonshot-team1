79e52e5d6bcf20ce7e100cab5af938c083913fbb
Minsoo Choi <chamysj0429@gmail.com>
2026-01-09 11:08:50 +0900

    🛠️ refactor : 리베이스 컴파일 에러 수정

--- src/types/task.ts
+++ src/types/task.ts
@@ -3,13 +3,11 @@ import { Prisma } from '@prisma/client';
 export const taskInclude = {
   assigneeProjectMember: { include: { member: true } },
   taskTags: { include: { tag: true } },
-  attachments: true,
+  attachments: true
 } satisfies Prisma.TaskInclude;
-
 
 // DB에서 꺼낸 타입
 export type TaskWithDetails = Prisma.TaskGetPayload<{ include: typeof taskInclude }>;
-
 
 // task 생성 input 타입
 export interface TaskInput {
@@ -24,9 +22,8 @@ export interface TaskInput {
   status?: string;
   assigneeId?: number | string;
   tags?: string[]; // 문자열 배열이거나, 멀티파트일 땐 문자열일 수 있음
-  attachments?: string[]
+  attachments?: string[];
 }
-
 
 // task 목록 조회 input 타입
 export interface TaskQueryInput {
@@ -38,3 +35,12 @@ export interface TaskQueryInput {
   order?: 'asc' | 'desc';
   order_by?: 'created_at' | 'name' | 'end_date';
 }
+//민수 추가
+export type TaskDelete = Prisma.TaskGetPayload<{
+  select: {
+    id: true;
+    projectId: true;
+    taskCreatorId: true;
+    googleEventId: true;
+  };
+}>;
