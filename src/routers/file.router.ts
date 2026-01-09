6065833f894402f65c24dc2e3e838d25ad417bc4
geonsoon <qkrrjstns23@gmail.com>
2026-01-09 16:18:52 +0900

    ♻️ chore(update) : /files 부분 코드 수정

--- src/routers/file.router.ts
+++ src/routers/file.router.ts
@@ -1,23 +1,17 @@
 import express from 'express';
-import { upload } from '../middlewares/upload.js';
-import { authenticate } from '../middlewares/authenticate.js';
-import authorize from '../middlewares/authorize.js';
-import * as fileControl from '../controllers/file.control.js';
-import { asyncHandler } from '../middlewares/asyncHandler.js';
+import { uploadProfile, uploadTaskFile } from '../middlewares/upload';
+import * as fileControl from '../controllers/file.control';
+import { asyncHandler } from '../middlewares/asyncHandler';
+
+
 
 const fileRouter = express.Router();
 
-/**
- * [1] 회원가입용 (단일 파일, 인증 없음)
- * POST /files/public
- */
-fileRouter.post('/public', upload.single('image'), asyncHandler(fileControl.uploadSingle));
+// 할 일 첨부파일 업로드 (name="files"로 약속)
+fileRouter.post('/', uploadTaskFile.single('files'), asyncHandler(fileControl.uploadSingleFile));
 
-/**
- * [2] 할 일 첨부용 (다중 파일, 인증+권한 필수)
- * POST /files/projects/:projectId
- */
-fileRouter.post('/projects/:projectId', authenticate, authorize.projectMember, upload.array('image'), asyncHandler(fileControl.uploadMultiple));
+// 프로필 이미지 업로드 (name="files"로 약속)
+fileRouter.post('/profile', uploadProfile.single('files'), asyncHandler(fileControl.uploadSingleFile));
 
 export default fileRouter;
 
