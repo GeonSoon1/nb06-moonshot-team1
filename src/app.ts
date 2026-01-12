a210c553cbeb239e72ab9222cf57344782968308
Minsoo Choi <chamysj0429@gmail.com>
2026-01-12 21:53:43 +0900

    ✅ test : 환경 변수 테스트

--- src/app.ts
+++ src/app.ts
@@ -18,7 +18,7 @@ const app = express();
 
 app.use(
   cors({
-    origin: process.env.FRONTEND_URL || 'http://localhost:3000',
+    origin: process.env.FRONTEND_URL?.trim() || 'http://localhost:3000',
     credentials: true,
     allowedHeaders: ['Content-Type', 'Authorization']
   })
