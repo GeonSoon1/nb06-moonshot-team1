b25370e90b406cd92c6d408659e65058a1582653
Minsoo Choi <chamysj0429@gmail.com>
2026-01-12 17:07:15 +0900

    📝 docs : 자잘한 주석 수정

--- dist/types/task.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.taskInclude = void 0;
-exports.taskInclude = {
-    assigneeProjectMember: { include: { member: true } },
-    taskTags: { include: { tag: true } },
-    attachments: true
-};
